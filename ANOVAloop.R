b9b80e41c21a2ad82c9526dae159fa6fafcf5189
Fan Wang <fan1@uchicago.edu>
2016-05-10 22:00:14 -0500

    Update ANOVAloop.R

--- ANOVAloop.R
+++ ANOVAloop.R
@@ -2,12 +2,9 @@ a=read.csv("SUBTYPE_expression.csv")
 sink("AnovaTest.csv")
 for(i in 3:137){
   columns <- names(a[i])
-  anovaresult<- anova(aov(a[,i]~subtype,data=a)) 
-  posthocresult <- TukeyHSD(aov(a[,i]~subtype,data=a))
-  
+  anovaresult<- anova(lm(a[,i]~subtype,data=a)) 
   print(columns)
   print(anovaresult)
-  print(posthocresult)
 }
 sink()        ###print all results including significant and non-significant###
 
@@ -17,12 +14,10 @@ sink()        ###print all results including significant and non-significant###
 sink("significantAnovaTest.csv")
 for(i in 3:137){
   columns <- names(a[i])
-  anovaresult<- anova(aov(a[,i]~subtype,data=a)) 
-  if(anovaresult$Pr[1] < 0.05){ print(anovaresult) }
-  posthocresult <- TukeyHSD(aov(a[,i]~subtype,data=a))
-  
+  anovaresult<- anova(lm(a[,i]~subtype,data=a)) 
+  if(anovaresult$Pr[1] < 0.05){ 
   print(columns)
+  print(anovaresult$Pr) }
 
-  print(posthocresult)
 }
 sink()  ###print significant results including ###
