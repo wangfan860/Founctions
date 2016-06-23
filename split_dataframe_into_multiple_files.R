ac7825cf46fba7a07f6e290aaab0617f82730cdf
Fan Wang <fan1@uchicago.edu>
2016-06-23 11:01:25 -0500

    Update split_dataframe_into_multiple_files.R

--- split_dataframe_into_multiple_files.R
+++ split_dataframe_into_multiple_files.R
@@ -1,3 +1,3 @@
 a= read.delim("BRCA.mirnaseq_illuminahiseq_miR_isoform.quantification.txt",header = TRUE, sep = "\t")
 spt1 <- split(a, a$SampleId) 
-lapply(names(spt1), function(x){write.csv(spt1[[x]], file = paste(x,sep = ""),row.names=FALSE)})
+lapply(names(spt1), function(x){write.table(spt1[[x]], file = paste(x,"_isoform.quantification",sep = ""),row.names=FALSE,sep = "\t")})
