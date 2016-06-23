a32bd818b94292782b7ce1f6af76c1c4de4bf18e
Fan Wang <fan1@uchicago.edu>
2016-06-23 11:09:41 -0500

    add cmd code

--- split_dataframe_into_multiple_files.R
+++ split_dataframe_into_multiple_files.R
@@ -1,3 +1,7 @@
 a= read.delim("BRCA.mirnaseq_illuminahiseq_miR_isoform.quantification.txt",header = TRUE, sep = "\t")
 spt1 <- split(a, a$SampleId) 
 lapply(names(spt1), function(x){write.table(spt1[[x]], file = paste(x,"_isoform.quantification",sep = ""),row.names=FALSE,sep = "\t")})
+
+##change all file into txt file
+###in cmd window type cd C:\Users\fwang1\Desktop\BRCA_Survival\hiseq2             
+##rename files ren * *.txt
