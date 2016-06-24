2a80d5cf157c1b0d0d112bff49c7ace5b6c34fae
Fan Wang <fan1@uchicago.edu>
2016-06-24 08:40:17 -0500

    Update Remove_duplicated_rows.r

--- Remove_duplicated_rows.r
+++ Remove_duplicated_rows.r
@@ -1,4 +1,4 @@
 a=read.table("849BRCA.mirnaseq__illuminahiseq_miR_isoform.quantification.txt", sep="\t",header=T)
 b=data.frame(a[,7])
 c=b[!duplicated(b), ]
-write.csv("849-unique_miR.csv)
+write.csv(c,"849-unique_miR.csv)
