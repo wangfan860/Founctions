2cf34e573f8df9e3ea877c58e627c5dfe1b8c891
Fan Wang <fan1@uchicago.edu>
2021-06-09 23:23:22 -0500

    Delete gardner.bash

--- gardner.bash
+++
@@ -1,9 +0,0 @@
-ssh -l fwang1 gardner.cri.uchicago.edu 
-cd /group
-cd /scratch
-module avail
-module load gcc/6.2.0
-module list
-module keyword python
-module load R/3.4.1
-module load python/3.6.0
