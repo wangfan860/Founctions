d7c5ba4dc998101918f360719915466b9ebe1e68
Fan Wang <fan1@uchicago.edu>
2021-06-09 23:21:20 -0500

    Delete FloatingBars.r

--- FloatingBars.r
+++
@@ -1,7 +0,0 @@
-d = data.frame(x  = c(1:116), y  = runif(116, min=0, max=100), z=runif(116, min=0, max=134),a=runif(116, min=0, max=60))
-
-df  = transform(d, mean=rowMeans(d), sd=apply(d,1, sd))
-x=df[1]
-mean=df[5]
-sd=df[6]
-qplot(x,mean)+geom_errorbar(aes(x=x, ymin=mean-sd, ymax=mean+sd), width=0.25)
