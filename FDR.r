1e27d40ec974caff9e294fad8a84e3137edcdfd8
Fan Wang <fan1@uchicago.edu>
2021-06-09 23:20:42 -0500

    Delete FDR.r

--- FDR.r
+++
@@ -1,7 +0,0 @@
-library("fdrtool") # load "fdrtool" library and p-values
-data(pvalues)
-pval = read.table("pvaluemirmrna.txt",sep='\t', header=T, quote = "\"", stringsAsFactors = F)
-pvalues= pval[,1]
-fdr = fdrtool(pvalues, statistic="pvalue") # estimate fdr and Fdr from p-values
-fdr$lfdr # estimated local fdr
-write.csv(fdr$lfdr, file="FDR.csv")
